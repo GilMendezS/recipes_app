ab9a0eea46ac854b2496c679bb3eb9a0c3e50a39
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-05-16 17:38:33 -0500

    fetch and update categories

--- api/controllers/category.js
+++ api/controllers/category.js
@@ -16,3 +16,39 @@ exports.addCategory = async (req, res, next) => {
         data: category
     })
 }
+exports.getCategory = async (req, res, next) => {
+    try {
+        const categoryId = req.params.id;
+        const category = await Category.findById(categoryId);
+        return res.status(200).json({
+            success: true,
+            data: category
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error fetching the category',
+            success: false,
+            error
+        })
+    }
+}
+exports.updateCategory = async (req, res, next) => {
+    try {
+        const categoryId = req.params.id;
+        const updatedCategory = await Category.findByIdAndUpdate(categoryId, req.body);
+        return res.status(200).json({
+            message: 'Category was modified',
+            success: true,
+            data: updatedCategory
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error modifying the category',
+            success: false,
+            error
+        })
+    }
+}
+exports.removeCategory = async (req, res, next) => {
+    
+}
