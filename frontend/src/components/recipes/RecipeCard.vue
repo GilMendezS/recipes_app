1827bbc5f87b39ed2f546f20ff77bd41e11d53b8
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-05-28 13:47:45 -0500

    lsiting recipes

---
+++ frontend/src/components/recipes/RecipeCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card red darken-2 with-margin">
+        <div class="card-content white-text">
+          <span class="card-title">{{recipe.name}}</span>
+          <p></p>
+        </div>
+        <div class="card-action">
+          <a href="#">See</a>
+        </div>
+    </div>
+</template>
+<style scoped>
+    
+</style>
+
+<script>
+export default {
+    props: ['recipe']
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="row">
+    <v-card-recipe :recipe="recipe" class="col s12 m6" v-for="recipe in recipes" :key="recipe._id">
+
+    </v-card-recipe>
   </div>
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import RecipeCard from '@/components/recipes/RecipeCard.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    'v-card-recipe': RecipeCard
+  },
+  data: () => ({
+    recipes: []
+  }),
+  mounted(){
+    this.getRecipes();
+  },
+  methods: {
+    getRecipes(){
+      fetch('http://localhost:3001/api/recipes')
+      .then(res  => res.json())
+      .then(response => {
+        
+        this.recipes = response.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
   }
 }
 </script>
